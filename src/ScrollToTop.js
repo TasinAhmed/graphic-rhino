c9796a20ca742a127f5216d5f4ede2c9dc23af43
Tasin Ahmed <atasin1998@gmail.com>
2020-09-05 01:34:46 -0400

    Added contact form

--- src/ScrollToTop.js
+++ src/ScrollToTop.js
@@ -1,19 +1,12 @@
-// https://stackoverflow.com/questions/36904185/react-router-scroll-to-top-on-every-transition
-
 import { useEffect } from "react";
-import { withRouter } from "react-router-dom";
+import { useLocation } from "react-router-dom";
 
-function ScrollToTop({ history }) {
+export default function ScrollToTop() {
+  const { pathname } = useLocation();
+
   useEffect(() => {
-    const unlisten = history.listen(() => {
-      window.scrollTo(0, 0);
-    });
-    return () => {
-      unlisten();
-    };
-  }, []);
+    window.scrollTo(0, 0);
+  }, [pathname]);
 
   return null;
 }
-
-export default withRouter(ScrollToTop);
--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -2,6 +2,8 @@ import React from "react";
 import Header from "../../components/Header/Header";
 import Background from "../../images/contact.png";
 import { Helmet } from "react-helmet";
+import Map from "../../images/map.png";
+import Button from "../../components/Button/Button";
 
 const title = "Contacteer ons";
 const text =
@@ -14,6 +16,40 @@ function Contact() {
         <title>Graphic Rhino | Contact</title>
       </Helmet>
       <Header image={Background} title={title} text={text} />
+      <main>
+        <section className="information">
+          <div className="container">
+            <img className="information__map" src={Map} alt="Map" />
+            <form className="form">
+              <input
+                className="form__input"
+                required
+                type="text"
+                placeholder="Naam + achternaam*"
+              />
+              <input
+                className="form__input"
+                required
+                type="email"
+                placeholder="Email*"
+              />
+              <input
+                className="form__input"
+                required
+                type="tel"
+                placeholder="Telefoonnummer"
+              />
+              <input
+                className="form__input"
+                required
+                type="email"
+                placeholder="Bericht"
+              />
+              <Button btnText="verzenden" />
+            </form>
+          </div>
+        </section>
+      </main>
     </div>
   );
 }
