4fb74c8a58634f346fa183fe7bdba53428bb953a
Tasin Ahmed <atasin1998@gmail.com>
2020-09-08 11:54:44 -0400

    Updated menu

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,10 +1,14 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
+import Logo from "../../images/logo-menu.png";
 
 function Menu() {
   const closeMenu = () => {
     document.getElementById("menu-btn").click();
   };
+
+  let location = useLocation();
+  console.log(location.pathname);
 
   return (
     <div className="menu">
@@ -13,31 +17,35 @@ function Menu() {
         <label for="menu-checkbox" className="menu__btn" id="menu-btn">
           <div className="menu__icon"></div>
         </label>
-        <div className="menu__background"></div>
-        <div className="menu__nav">
-          <ul className="menu__list">
-            <Link to="/" onClick={closeMenu}>
-              <li>Home</li>
-            </Link>
-            <Link to="/about" onClick={closeMenu}>
-              <li>Over ons</li>
-            </Link>
-            <Link to="/diensten" onClick={closeMenu}>
-              <li>Diensten</li>
-            </Link>
-            <Link to="/cases" onClick={closeMenu}>
-              <li>Cases</li>
-            </Link>
-            <Link to="/blog" onClick={closeMenu}>
-              <li>Blog</li>
-            </Link>
-            <Link to="/graveyard" onClick={closeMenu}>
-              <li>Graveyard</li>
-            </Link>
-            <Link to="/contact" onClick={closeMenu}>
-              <li>Contact</li>
-            </Link>
-          </ul>
+        <div className="menu__background">
+          <div className="container">
+            <img className="menu__logo" src={Logo} alt="Menu Logo" />
+            <div className="menu__nav">
+              <ul className="menu__list">
+                <Link to="/" onClick={closeMenu}>
+                  <li className="active">Home</li>
+                </Link>
+                <Link to="/about" onClick={closeMenu}>
+                  <li>Over ons</li>
+                </Link>
+                <Link to="/diensten" onClick={closeMenu}>
+                  <li>Diensten</li>
+                </Link>
+                <Link to="/cases" onClick={closeMenu}>
+                  <li>Cases</li>
+                </Link>
+                <Link to="/blog" onClick={closeMenu}>
+                  <li>Blog</li>
+                </Link>
+                <Link to="/graveyard" onClick={closeMenu}>
+                  <li>Graveyard</li>
+                </Link>
+                <Link to="/contact" onClick={closeMenu}>
+                  <li>Contact</li>
+                </Link>
+              </ul>
+            </div>
+          </div>
         </div>
       </div>
     </div>
