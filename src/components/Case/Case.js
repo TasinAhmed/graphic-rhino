0f6985fea1c060925d183caffee9fc2c8fe904c6
Tasin Ahmed <atasin1998@gmail.com>
2020-09-10 13:23:19 -0400

    Added graveyard

--- src/components/Case/Case.js
+++ src/components/Case/Case.js
@@ -1,9 +1,18 @@
 import React from "react";
+import Button from "../Button/Button";
 
-function Case() {
+function Case({ title, desc, image, mirror = false }) {
   return (
-    <div>
-      <h1>Hello</h1>
+    <div className={`case ${mirror ? "case--mirror" : ""}`}>
+      <div className="case__content">
+        <div className="case__title">{title}</div>
+        <div className="case__desc">{desc}</div>
+        <Button btnText="bekijk de case" />
+      </div>
+      <div
+        className="case__img"
+        style={{ backgroundImage: `url(${image})` }}
+      ></div>
     </div>
   );
 }
--- src/pages/Graveyard/Graveyard.js
+++ src/pages/Graveyard/Graveyard.js
@@ -2,10 +2,14 @@ import React, { useRef } from "react";
 import Header from "../../components/Header/Header";
 import Background from "../../images/graveyard.png";
 import { Helmet } from "react-helmet";
+import Case from "../../components/Case/Case";
+import Image from "../../images/explore-1.png";
 
 const title = "Design graveyard";
 const text =
   "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam.";
+const desc =
+  "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. ";
 
 function Graveyard() {
   const myRef = useRef(null);
@@ -22,7 +26,16 @@ function Graveyard() {
         link="/cases"
         scrollTo={myRef}
       />
-      <main className="graveyard" ref={myRef}></main>
+      <main className="graveyard" ref={myRef}>
+        <div className="container">
+          <Case image={Image} title="Case title" desc={desc} />
+          <Case image={Image} mirror={true} title="Case title" desc={desc} />
+          <Case image={Image} title="Case title" desc={desc} />
+          <Case image={Image} mirror={true} title="Case title" desc={desc} />
+          <Case image={Image} title="Case title" desc={desc} />
+          <Case image={Image} mirror={true} title="Case title" desc={desc} />
+        </div>
+      </main>
     </>
   );
 }
