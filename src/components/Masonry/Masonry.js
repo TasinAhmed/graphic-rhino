d7dcd53f4caf40e3470bcbd8fdeaddf00bc04404
Tasin Ahmed <atasin1998@gmail.com>
2020-09-13 20:12:51 -0400

    Updated masonry grid

--- src/components/Masonry/Masonry.js
+++ src/components/Masonry/Masonry.js
@@ -1,18 +1,26 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Masonry({ images }) {
   const items = [];
   for (const x of images) {
     items.push(
-      <div className="masonry__block">
-        <img className="masonry__img" alt="Grid Item" src={x.url} />
-        <div className="masonry__cover"></div>
-        <div className="masonry__content">
-          <div className="masonry__text">
-            <div className="masonry__title">{x.title}</div>
+      <Link>
+        <div className="masonry__block">
+          <img className="masonry__img" alt="Grid Item" src={x.url} />
+          <div className="masonry__cover"></div>
+          <div className="masonry__content">
+            <div className="masonry__bar masonry__bar--1"></div>
+            <div className="masonry__bar masonry__bar--2"></div>
+            <div className="masonry__text">
+              <div className="masonry__title">{x.title}</div>
+              <div className="masonry__desc">{x.desc}</div>
+            </div>
+            <div className="masonry__bar masonry__bar--3"></div>
+            <div className="masonry__bar masonry__bar--4"></div>
           </div>
         </div>
-      </div>
+      </Link>
     );
   }
   console.log(items);
--- src/pages/Blog/Images.js
+++ src/pages/Blog/Images.js
@@ -2,118 +2,164 @@ const Images = [
   {
     url:
       "https://images.unsplash.com/photo-1599687350916-a8be7f9b68f1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80",
-    title: "blog title",
+    title: "case title",
+    desc:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed dignissim.",
   },
   {
     url:
       "https://images.unsplash.com/photo-1599667228951-835a7198c2a9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80",
-    title: "blog title",
+    title: "case title",
+    desc:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed dignissim.",
   },
   {
     url:
       "https://images.unsplash.com/photo-1582472069132-0cd3678af233?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=618&q=80",
-    title: "blog title",
+    title: "case title",
+    desc:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed dignissim.",
   },
   {
     url:
       "https://images.unsplash.com/photo-1535902677902-a9ce6e79193a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80",
-    title: "blog title",
+    title: "case title",
+    desc:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed dignissim.",
   },
   {
     url:
       "https://images.unsplash.com/photo-1599677100002-bc1b29f4a02a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80",
-    title: "blog title",
+    title: "case title",
+    desc:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed dignissim.",
   },
   {
     url:
       "https://images.unsplash.com/photo-1599068197877-f833ce8741a2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=700&q=80",
-    title: "blog title",
+    title: "case title",
+    desc:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed dignissim.",
   },
   {
     url:
       "https://images.unsplash.com/photo-1599727915093-7524e6d0808f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80",
-    title: "blog title",
+    title: "case title",
+    desc:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed dignissim.",
   },
   {
     url:
       "https://images.unsplash.com/photo-1599770792456-229af73c466e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=676&q=80",
-    title: "blog title",
+    title: "case title",
+    desc:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed dignissim.",
   },
   {
     url:
       "https://images.unsplash.com/photo-1599811144484-f4a3610aa576?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=633&q=80",
-    title: "blog title",
+    title: "case title",
+    desc:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed dignissim.",
   },
 
   {
     url:
       "https://images.unsplash.com/photo-1593642634524-b40b5baae6bb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1489&q=80",
-    title: "blog title",
+    title: "case title",
+    desc:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed dignissim.",
   },
   {
     url:
       "https://images.unsplash.com/photo-1599804222394-eeaa7d1b41b1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80",
-    title: "blog title",
+    title: "case title",
+    desc:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed dignissim.",
   },
   {
     url:
       "https://images.unsplash.com/photo-1593643946890-b5b85ade6451?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1529&q=80",
-    title: "blog title",
+    title: "case title",
+    desc:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed dignissim.",
   },
   {
     url:
       "https://images.unsplash.com/photo-1599780196583-9c794405efc7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80",
-    title: "blog title",
+    title: "case title",
+    desc:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed dignissim.",
   },
   {
     url:
       "https://images.unsplash.com/photo-1599768481769-05d0c1962e99?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=700&q=80",
-    title: "blog title",
+    title: "case title",
+    desc:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed dignissim.",
   },
   {
     url:
       "https://images.unsplash.com/photo-1599790772272-d1425cd3242e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=700&q=80",
-    title: "blog title",
+    title: "case title",
+    desc:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed dignissim.",
   },
   {
     url:
       "https://images.unsplash.com/photo-1599836407497-be53adcad777?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=658&q=80",
-    title: "blog title",
+    title: "case title",
+    desc:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed dignissim.",
   },
   {
     url:
       "https://images.unsplash.com/photo-1599837035695-f679beb91920?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80",
-    title: "blog title",
+    title: "case title",
+    desc:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed dignissim.",
   },
   {
     url:
       "https://images.unsplash.com/photo-1599836705826-b3f962acaa10?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
-    title: "blog title",
+    title: "case title",
+    desc:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed dignissim.",
   },
   {
     url:
       "https://images.unsplash.com/photo-1599813405394-863bf27e1a32?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=649&q=80",
-    title: "blog title",
+    title: "case title",
+    desc:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed dignissim.",
   },
   {
     url:
       "https://images.unsplash.com/photo-1599826892560-268f52bc2c05?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80",
-    title: "blog title",
+    title: "case title",
+    desc:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed dignissim.",
   },
   {
     url:
       "https://images.unsplash.com/photo-1599765625246-5c661338b722?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=717&q=80",
-    title: "blog title",
+    title: "case title",
+    desc:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed dignissim.",
   },
   {
     url:
       "https://images.unsplash.com/photo-1599687349533-82f24a0b62cb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80",
-    title: "blog title",
+    title: "case title",
+    desc:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed dignissim.",
   },
   {
     url:
       "https://images.unsplash.com/photo-1599841934376-1896a80aeace?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80",
-    title: "blog title",
+    title: "case title",
+    desc:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed dignissim.",
   },
 ];
 
