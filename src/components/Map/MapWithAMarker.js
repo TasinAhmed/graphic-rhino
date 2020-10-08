c9313dc18d55d6213f4dfc34502499e8c44273af
Tasin Ahmed <atasin1998@gmail.com>
2020-10-08 01:15:38 -0400

    added greyscale map

--- src/components/Map/MapWithAMarker.js
+++ src/components/Map/MapWithAMarker.js
@@ -8,85 +8,94 @@ import {
 
 const defaultMapOptions = [
   {
-    elementType: "geometry",
+    featureType: "water",
+    elementType: "geometry.fill",
     stylers: [
       {
-        color: "#f5f5f5",
+        color: "#d3d3d3",
       },
     ],
   },
   {
-    elementType: "labels.icon",
+    featureType: "transit",
     stylers: [
+      {
+        color: "#808080",
+      },
       {
         visibility: "off",
       },
     ],
   },
   {
-    elementType: "labels.text.fill",
+    featureType: "road.highway",
+    elementType: "geometry.stroke",
     stylers: [
       {
-        color: "#616161",
+        visibility: "on",
+      },
+      {
+        color: "#b3b3b3",
       },
     ],
   },
   {
-    elementType: "labels.text.stroke",
+    featureType: "road.highway",
+    elementType: "geometry.fill",
     stylers: [
       {
-        color: "#f5f5f5",
+        color: "#ffffff",
       },
     ],
   },
   {
-    featureType: "administrative.land_parcel",
-    elementType: "labels.text.fill",
+    featureType: "road.local",
+    elementType: "geometry.fill",
     stylers: [
       {
-        color: "#bdbdbd",
+        visibility: "on",
+      },
+      {
+        color: "#ffffff",
+      },
+      {
+        weight: 1.8,
+      },
+    ],
+  },
+  {
+    featureType: "road.local",
+    elementType: "geometry.stroke",
+    stylers: [
+      {
+        color: "#d7d7d7",
       },
     ],
   },
   {
     featureType: "poi",
+    elementType: "geometry.fill",
+    stylers: [
+      {
+        visibility: "on",
+      },
+      {
+        color: "#ebebeb",
+      },
+    ],
+  },
+  {
+    featureType: "administrative",
     elementType: "geometry",
     stylers: [
       {
-        color: "#eeeeee",
+        color: "#a7a7a7",
       },
     ],
   },
   {
-    featureType: "poi",
-    elementType: "labels.text.fill",
-    stylers: [
-      {
-        color: "#757575",
-      },
-    ],
-  },
-  {
-    featureType: "poi.park",
-    elementType: "geometry",
-    stylers: [
-      {
-        color: "#e5e5e5",
-      },
-    ],
-  },
-  {
-    featureType: "poi.park",
-    elementType: "labels.text.fill",
-    stylers: [
-      {
-        color: "#9e9e9e",
-      },
-    ],
-  },
-  {
-    featureType: "road",
-    elementType: "geometry",
+    featureType: "road.arterial",
+    elementType: "geometry.fill",
     stylers: [
       {
         color: "#ffffff",
@@ -95,73 +104,89 @@ const defaultMapOptions = [
   },
   {
     featureType: "road.arterial",
-    elementType: "labels.text.fill",
+    elementType: "geometry.fill",
     stylers: [
       {
-        color: "#757575",
+        color: "#ffffff",
       },
     ],
   },
   {
-    featureType: "road.highway",
-    elementType: "geometry",
+    featureType: "landscape",
+    elementType: "geometry.fill",
+    stylers: [
+      {
+        visibility: "on",
+      },
+      {
+        color: "#efefef",
+      },
+    ],
+  },
+  {
+    featureType: "road",
+    elementType: "labels.text.fill",
+    stylers: [
+      {
+        color: "#696969",
+      },
+    ],
+  },
+  {
+    featureType: "administrative",
+    elementType: "labels.text.fill",
+    stylers: [
+      {
+        visibility: "on",
+      },
+      {
+        color: "#737373",
+      },
+    ],
+  },
+  {
+    featureType: "poi",
+    elementType: "labels.icon",
+    stylers: [
+      {
+        visibility: "off",
+      },
+    ],
+  },
+  {
+    featureType: "poi",
+    elementType: "labels",
+    stylers: [
+      {
+        visibility: "off",
+      },
+    ],
+  },
+  {
+    featureType: "road.arterial",
+    elementType: "geometry.stroke",
+    stylers: [
+      {
+        color: "#d6d6d6",
+      },
+    ],
+  },
+  {
+    featureType: "road",
+    elementType: "labels.icon",
+    stylers: [
+      {
+        visibility: "off",
+      },
+    ],
+  },
+  {},
+  {
+    featureType: "poi",
+    elementType: "geometry.fill",
     stylers: [
       {
         color: "#dadada",
-      },
-    ],
-  },
-  {
-    featureType: "road.highway",
-    elementType: "labels.text.fill",
-    stylers: [
-      {
-        color: "#616161",
-      },
-    ],
-  },
-  {
-    featureType: "road.local",
-    elementType: "labels.text.fill",
-    stylers: [
-      {
-        color: "#9e9e9e",
-      },
-    ],
-  },
-  {
-    featureType: "transit.line",
-    elementType: "geometry",
-    stylers: [
-      {
-        color: "#e5e5e5",
-      },
-    ],
-  },
-  {
-    featureType: "transit.station",
-    elementType: "geometry",
-    stylers: [
-      {
-        color: "#eeeeee",
-      },
-    ],
-  },
-  {
-    featureType: "water",
-    elementType: "geometry",
-    stylers: [
-      {
-        color: "#c9c9c9",
-      },
-    ],
-  },
-  {
-    featureType: "water",
-    elementType: "labels.text.fill",
-    stylers: [
-      {
-        color: "#9e9e9e",
       },
     ],
   },
@@ -170,8 +195,8 @@ const defaultMapOptions = [
 const MapWithAMarker = withScriptjs(
   withGoogleMap(({ lat, lng }) => (
     <GoogleMap
-      defaultZoom={18}
-      defaultCenter={{ lat: lat, lng: lng + 0.001 }}
+      defaultZoom={14}
+      defaultCenter={{ lat: lat, lng: lng + 0.01 }}
       options={{
         styles: defaultMapOptions,
         disableDefaultUI: true,
