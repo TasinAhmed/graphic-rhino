41c6888cd64c11c4b2ba065836765d1aaf060ef2
Tasin Ahmed <atasin1998@gmail.com>
2020-11-06 20:26:35 -0500

    Updated case images

--- src/pages/Cases/Images.js
+++ src/pages/Cases/Images.js
@@ -10,53 +10,53 @@ const Images = [
     desc: "",
   },
   {
-    url: "hhjpreview2.png",
+    url: "hhjpreview2.jpg",
     title: "HAPPY HANDMADE JEWELS, OUTRIJVE",
     desc: "",
   },
   {
-    url: "keurslagerpreview2.png",
+    url: "keurslagerpreview2.jpg",
     title: "KEURSLAGER CRUL, MOEN",
     desc: "",
   },
   {
-    url: "ebopreview.png",
+    url: "ebopreview.jpg",
     title: "EBO, INGOOIGEM",
     desc: "",
   },
   {
-    url: "portfolio1.png",
+    url: "portfolio1.jpg",
     title: "CAFETARIA MICHELSBERG, SPIERE-HELKIJN",
     desc: "",
   },
   {
-    url: "portfolio2.png",
+    url: "portfolio2.jpg",
     title: "PITTA MELITA, AVELGEM",
     desc: "",
   },
   {
-    url: "portfolio3.png",
+    url: "portfolio3.jpg",
     title: "MALAMUTE MATTERS, GROOT-BRITTANNIË",
     desc: "",
   },
   {
-    url: "portfolio4.png",
+    url: "portfolio4.jpg",
     title: "EECKHOUT - WOODLINE, OOSTROZEBEKE",
     desc: "",
   },
 
   {
-    url: "portfolio5.png",
+    url: "portfolio5.jpg",
     title: "SLAGERIJ CANNAERT, ANZEGEM",
     desc: "",
   },
   {
-    url: "portfolio6.png",
+    url: "portfolio6.jpg",
     title: "HET LIJSTERNEST, INGOOIGEM",
     desc: "",
   },
   {
-    url: "portfolio7.png",
+    url: "portfolio7.jpg",
     title: "BLACK - DIRK BRACKE",
     desc: "",
   },
